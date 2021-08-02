b74490f00edb2250dd256ac67053fb7607f652b6
JuliusStein <juliusastein@gmail.com>
2021-08-02 11:58:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Installing this package (Mac/Linux)
+To install this package and its dependencies in a new conda environment, simply run:
+```shell
+conda create -n week2 python=3.8
+conda activate week2
+
+(week2) pip install -e cogworks_week2
+```
+
+# Installing this package (PC)
+To install this package and its dependencies in a new conda environment, simply run:
+```shell
+conda create -n week2 python=3.8 ipython jupyter notebook numpy scipy matplotlib pyaudio numba
+conda install -n week2 -c conda-forge librosa ffmpeg
+conda activate week2
+
+(week 2) pip install -e cogworks_week2
+```
---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cogworks_week2",
+    version="1.0.0",
+    author="Ryan Soklaski (@rsokl)",
+    author_email="ry26099@mit.edu",
+    url="https://github.com/JuliusStein/Week2_pip",
+    description="Provides resources and required libraries for the Vision/Video Module of CogWorks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license = 'MIT',
+    zip_safe = False,
+    install_requires=[
+        "ipython",
+        "jupyter",
+        "notebook",
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "numba",
+        "bottleneck",
+        "xarray",
+        "mygrad",
+        "mynn",
+        "noggin",
+        "facenet-pytorch"
+    ],
+    packages=['camera', 'datasets', 'facenet-models'],
+    python_requires=">=3.6",
+)
